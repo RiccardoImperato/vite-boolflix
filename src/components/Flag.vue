ae3eb085a1aa33c612b56db7db4e7e4242b7f415
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-14 22:38:35 +0100

    feat: componente Flag

---
+++ src/components/Flag.vue
@@ -0,0 +1,20 @@
+<script >
+
+export default {
+    props: {
+        lang: String
+    }
+}
+</script>
+
+<template>
+    <div>
+        <div>Lingua originale: <span>{{ lang }}</span></div>
+    </div>
+</template>
+
+<style scoped>
+span {
+    text-transform: uppercase;
+}
+</style>
