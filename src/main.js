076c11aaa87559b05212d205f778a740530dbb8a
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-15 15:18:09 +0100

    font awsome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import '../src/assets/scss/style.scss'
 import App from './App.vue'
+// Font awsome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
 
-createApp(App).mount('#app')
+
+library.add(faStar, emptyStar)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
