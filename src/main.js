264273b76d6b4e569a21856a33b28687d96c5717
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-15 21:15:02 +0100

    font Montserrat

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import '../src/assets/scss/style.scss'
 import App from './App.vue'
+// Font Montserrat
+import "@fontsource/montserrat";
 // Font awsome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
+import { faImage } from '@fortawesome/free-regular-svg-icons'
 
-
-library.add(faStar, emptyStar)
+library.add(faStar, emptyStar, faImage)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
