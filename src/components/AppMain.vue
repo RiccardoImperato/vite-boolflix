69a22bfd6015f08c65e86c00af5a15c9bd772e5e
RiccardoImperato <riccardo.imperato@libero.it>
2023-12-15 18:20:56 +0100

    feat: milestone 3 + refactor

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,11 @@
 <script >
 import { store } from '../store';
-import MovieCard from './MovieCard.vue';
-import TvCard from './TvCard.vue';
+import Card from './Card.vue';
 import Flag from './Flag.vue';
 
 export default {
     components: {
-        MovieCard,
-        TvCard,
+        Card,
         Flag
     },
     data() {
@@ -20,14 +18,18 @@ export default {
 
 <template>
     <ul>
+
         <li v-for="movie in this.store.movieList">
-            <MovieCard :img="movie.poster_path" :title="movie.title" :ogTitle="movie.original_title"
+            <Card :img="movie.poster_path" :title="movie.title" :ogTitle="movie.original_title"
                 :rate="movie.vote_average" />
             <Flag :lang="movie.original_language" />
+            <h4>Movie</h4>
         </li>
+
         <li v-for="serie in this.store.tvList">
-            <TvCard :img="serie.poster_path" :name="serie.name" :ogName="serie.original_name" :rate="serie.vote_average" />
+            <Card :img="serie.poster_path" :title="serie.name" :ogTitle="serie.original_name" :rate="serie.vote_average" />
             <Flag :lang="serie.original_language" />
+            <h4>TV series</h4>
         </li>
     </ul>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,47 @@
+<script >
+
+export default {
+    props: {
+        title: String,
+        ogTitle: String,
+        lang: String,
+        rate: Number,
+        img: String
+    },
+    computed: {
+        getStars() {
+            return Math.ceil(this.rate / 2);
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div>
+            <h3>
+                {{ title }}
+            </h3>
+            <img v-if="img !== null" :src="'https://image.tmdb.org/t/p/w342' + img" :alt="title">
+        </div>
+        <div v-if="title !== ogTitle">
+            Titolo originale: <h4>{{ ogTitle }}</h4>
+        </div>
+        <ul>
+            <li v-for="n in getStars">
+                <font-awesome-icon icon="fa-solid fa-star" />
+            </li>
+            <li v-for="n in 5 - getStars">
+                <font-awesome-icon icon="fa-regular fa-star" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+ul {
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+}
+</style>
